73e9d84dc17adc417c9b682b1539408da83926f5
andrevictorbf <andrevictor_barcia@hotmail.com>
2024-05-22 07:31:43 -0300

    Excluisão
    Exclui a entidade venda

--- Estoque/src/Negocioacoes/Venda.java
+++ Estoque/src/Negocioacoes/Venda.java
@@ -14,6 +14,56 @@ public final class Venda extends Negociacao {
         this.comissaoVendedor = valorTotal * 0.05;
     }
 
+    @Override
+    public String getProdutoID() {
+        // TODO Auto-generated method stub
+        return super.getProdutoID();
+    }
+
+    @Override
+    public LocalDateTime getRealziacao() {
+        // TODO Auto-generated method stub
+        return super.getRealziacao();
+    }
+
+    @Override
+    public String getUserId() {
+        // TODO Auto-generated method stub
+        return super.getUserId();
+    }
+
+    @Override
+    public float getValorTotal() {
+        // TODO Auto-generated method stub
+        return super.getValorTotal();
+    }
+
+    @Override
+    public void setProdutoID(String produtoID) {
+        // TODO Auto-generated method stub
+        super.setProdutoID(produtoID);
+    }
+
+    @Override
+    public void setRealziacao(LocalDateTime realziacao) {
+        // TODO Auto-generated method stub
+        super.setRealziacao(realziacao);
+    }
+
+    @Override
+    public void setUserId(String userId) {
+        // TODO Auto-generated method stub
+        super.setUserId(userId);
+    }
+
+    @Override
+    public void setValorTotal(float valorTotal) {
+        // TODO Auto-generated method stub
+        super.setValorTotal(valorTotal);
+    }
+
+    
+
 
 
 }
