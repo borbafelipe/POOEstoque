c0ce7c684af93f80254d832067cd1e948874dfdc
Felipe Ricardo Tiberio Borba <149276247+borbafelipe@users.noreply.github.com>
2024-06-04 06:31:47 -0300

    Add files via upload
    Versão com interfaces implementadas

---
+++ estoqueFinal/estoque/src/Produto/Calcado.java
@@ -0,0 +1,11 @@
+package Produto;
+
+import Enum.NumCalcado;
+
+
+
+public interface Calcado {
+    void adicionarQuantidade(NumCalcado numCalcado, int quantidade);
+    void removerQuantidade(NumCalcado numCalcado, int quantidade);
+    int getQuantidade(NumCalcado numCalcado);
+}
